c57470a06c30ecf5f2e04b677068d368ceb3c584
Marie Nabors <marie.nabors@gmail.com>
2019-10-21 11:13:25 -0400

    setup components and weather service

---
+++ LovelyDay/src/app/weather.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(public http: HttpClient) {
+   }
+}
